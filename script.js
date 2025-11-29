d1e9a902bc6470100aaa116d481936c873851594
Akshaysinghparmar106 <parmarakshay973@gmail.com>
2025-11-29 20:14:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+	function sphereVolume(radius) {
+    const volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+    return volume;
+}
+
+// Example usage:
+console.log(sphereVolume(5)); 
+
   
 } 
 
